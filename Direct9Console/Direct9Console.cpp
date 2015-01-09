ecbbcf5aebe148e72900df264208a71fb7e4837a
Fabricio Catae <fabricio.catae@microsoft.com>
2015-01-09 18:47:42 -0200

    tested direct9console

--- Direct9Console/Direct9Console.cpp
+++ Direct9Console/Direct9Console.cpp
@@ -5,13 +5,20 @@
 
 typedef LPDIRECT3D9(WINAPI* PD3DCALL)(UINT);
 
+void Log(char* msg)
+{
+	printf(" - %s\n", msg);
+}
+
 void* GetDirect3DCreateFunc()
 {
+	Log("LoadModule D3D9.DLL");
 	HMODULE d3mod = LoadLibrary(L"D3D9.DLL");
 
 	if (d3mod == NULL)
 		return NULL;
 
+	Log("Load Function Direct3DCreate9");
 	return GetProcAddress(d3mod, "Direct3DCreate9");
 }
 
@@ -24,24 +31,25 @@ BOOL CheckDirectX()
 	if (Direct3DCreate9 == NULL)
 		return FALSE;
 
+	Log("Call Direct3DCreate9()");
 	g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
 
 	if (g_pD3D == NULL)
 		return FALSE;
 
-	if (FAILED(g_pD3D->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps)))
-		return FALSE;
+	Log("Call GetDeviceCaps()");
+	HRESULT hr = g_pD3D->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps);
 
-	return TRUE;
+	return SUCCEEDED(hr);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("Direct9Console");
-	char ch; scanf_s("%c", &ch);
+	printf("Direct9Console\n");
+
 	char *result = CheckDirectX() ? "OK" : "FAILED";
 
-	printf(" - CheckDirectX: %s", result);
+	printf("Result = %s\n", result);
 
 	return 0;
 }
