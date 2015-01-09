732b54cc83c6a9a53413ef65593a1c1fe3d05eb3
Fabricio Catae <fabricio.catae@microsoft.com>
2015-01-09 16:42:32 -0200

    add console

---
+++ Console/Console.cpp
@@ -0,0 +1,17 @@
+// Console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	string input;
+
+	cout << "Console...";
+	
+	 dcin >> input;
+
+	return 0;
+}
+
